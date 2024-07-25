da22c7ee4c9fb28cc210d9bd3f50981821ccdfb6
Shirosaaki <matheo.campanella@epitech.eu>
2024-07-25 14:24:26 +0200

    [-] delete file

--- main.c
+++
@@ -1,7 +0,0 @@
-#include "my.h"
-
-int main(void)
-{
-    func1000111("Test str");
-    return 0;
-}
--- my.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_H_
-	#define MY_H_
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <time.h>
-	#include <string.h>
-	#include <unistd.h>
-int func1000001 (int var000000) ;
-#endif /* !MY_H_ */
